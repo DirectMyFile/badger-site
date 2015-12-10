96cb3f5cff50b562f9c1489bd9eaef4ee761b58d
Kenneth Endfinger <kaendfinger@gmail.com>
2015-12-10 10:34:47 -0500

    Bug Fixes

--- tool/build.sh
+++ tool/build.sh
@@ -7,7 +7,7 @@ pub build
 BUILD_DIR="$(pwd)/build/web"
 
 [ -d "/tmp/badger-site" ] && rm -rf /tmp/badger-site
-git clone git@github.com:badger-lang/site.git /tmp/badger-site
+git clone https://github.com/badger-lang/site.git /tmp/badger-site
 pushd /tmp/badger-site
 git checkout gh-pages
 cp -R ${BUILD_DIR}/* .
